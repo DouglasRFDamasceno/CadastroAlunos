c5eb06888cbcfdfe0ff2bda0758a75bb16f7cfe1
Douglas <douglas.damasceno@tcsindustrial.com.br>
2020-10-24 02:17:03 -0300

    Atualização Código

--- App.js
+++ App.js
@@ -1,16 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState, useEffect } from 'react'
 import React from 'react';
-import { Text, View, Button, Alert } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import {Home, Login, Cadastro, Alunos} from './views'
+import {Home, Login, Alunos} from './views/Index';
+import AreaRestrita from './views/AreaRestrita';
 
 export default function App() {
-  const [nome, setNome] = useState(null);
-  const [endereco, setEndereco] = useState(null);
   const Stack = createStackNavigator();
 
   return (
@@ -19,15 +15,15 @@ export default function App() {
         <Stack.Screen 
         name="Home" 
         options={{
-          title:'Bem vindo!',
+          title:'Sistema de cadastro de alunos',
           headerTintColor:'#333',
           headerStyle:{backgroundColor: 'orange'},
           headerTitleStyle:{fontWeight:'bold', alignSelf:'center'},
         }}
         component={Home} />
         <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Cadastro" component={Cadastro} />
         <Stack.Screen name="Alunos" component={Alunos} />
+        <Stack.Screen name="AreaRestrita" component={AreaRestrita} />
       </Stack.Navigator>
     </NavigationContainer>
   );
